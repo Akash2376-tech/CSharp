6f7ff32be6a3257586983c4bc04c32c6306964bf
Akash2376-tech <68938585+Akash2376-tech@users.noreply.github.com>
2020-08-02 11:30:40 +0530

    Changes Added

---
+++ Utility/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+namespace Utility
+{
+    public class RelayCommand : ICommand
+    {
+        Action<object> _execMethod;
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> _exec)
+        {
+            _execMethod = _exec;
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (_execMethod != null)
+                return true;
+            else
+                return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execMethod(parameter);
+        }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+namespace ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void onPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
