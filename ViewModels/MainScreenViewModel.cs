74e0eca739838e38d853c4fc47d4ed5303da6da1
Akash2376-tech <68938585+Akash2376-tech@users.noreply.github.com>
2020-08-04 10:43:46 +0530

    Changes for Text to label on click button

---
+++ ViewModels/MainScreenViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ViewModels
+{
+    public class MainScreenViewModel:ViewModelBase
+    {
+        #region Private Variables
+        private string _text;
+        private ICommand _print;
+        #endregion
+        #region Public Variables
+        public string Text { get { return _text; } set { _text = value; onPropertyChanged("Text"); } }
+        public ICommand Print { get; set; }
+        #endregion
+        public MainScreenViewModel()
+        {
+
+        }
+        public void showData()
+        {
+
+        }
+
+    }
+}
