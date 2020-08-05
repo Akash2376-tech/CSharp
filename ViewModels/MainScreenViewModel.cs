1e8480f1c62d9121a91c5b7955936dc0ced9ce47
Akash2376-tech <68938585+Akash2376-tech@users.noreply.github.com>
2020-08-05 09:24:48 +0530

    Changes

--- ViewModels/MainScreenViewModel.cs
+++ ViewModels/MainScreenViewModel.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Windows.Input;
+using Utility;
+using System.Windows;
 
 namespace ViewModels
 {
     public class MainScreenViewModel:ViewModelBase
     {
         #region Private Variables
-        private string _text;
+        private string _text;        
         private ICommand _print;
         #endregion
-        #region Public Variables
+        #region Public Variables        
         public string Text { get { return _text; } set { _text = value; onPropertyChanged("Text"); } }
-        public ICommand Print { get; set; }
+        public ICommand Print { get { return _print ?? (_print = new RelayCommand(para => showData())); } set { _print = value; } }
         #endregion
         public MainScreenViewModel()
         {
@@ -21,7 +24,7 @@ namespace ViewModels
         }
         public void showData()
         {
-
+            MessageBox.Show(Text);
         }
 
     }
